18789ff6eef4e87e37ec70427d85c70e60ff93ca
piusgori <piusgori@gmail.com>
2022-06-29 21:16:16 +0300

    changes

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/files', express.static(path.join('images')));
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
